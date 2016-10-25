c4e5fed574574b8402fc9a61cdac580105db3da3
Suzumiya <jrgwyz@163.com>
2016-10-26 00:42:59 +0800

    init

---
+++ decode_pc_weichat.py
@@ -0,0 +1,18 @@
+import re
+
+
+def _decode_pc_dat(datfile):
+    with open(datfile, 'rb') as f:
+        buf = bytearray(f.read())
+
+    magic = 0xff ^ list(buf)[0] if buf else 0x00
+    print(magic)
+
+    imgfile = re.sub(r'.dat$', '.jpg', datfile)
+    with open(imgfile, 'wb') as f:
+        newbuf = bytearray(map(lambda b: b ^ magic, list(buf)))
+        f.write(newbuf)
+
+
+if __name__ == '__main__':
+    _decode_pc_dat(datfile='xxxxx.dat')
