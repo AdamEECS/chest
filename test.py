3ff7fe9e4898dd2d3ab82d0642da5cc33a8ef291
Suzumiya <jrgwyz@163.com>
2016-10-28 19:12:51 +0800

    init

---
+++ test.py
@@ -0,0 +1,14 @@
+towns = [
+    {'name': 'Manchester', 'population': 58241},
+    {'name': 'Coventry', 'population': 12435},
+    {'name': 'South Windsor', 'population': 25709}
+]
+
+print(list(zip(*[(t.get('name'), t.get('population')) for t in towns])))
+
+
+def get_value(key, lists):
+    return list(map(lambda t: t.get(key), lists))
+
+print(get_value('name', towns))
+print(get_value('population', towns))
