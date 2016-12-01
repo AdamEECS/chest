b04b914d67739655d254c8ef48d382751dd0d6e1
Suzumiya <jrgwyz@163.com>
2016-12-01 12:03:15 +0800

    md updated

--- test.py
+++ test.py
@@ -10,5 +10,6 @@ print(list(zip(*[(t.get('name'), t.get('population')) for t in towns])))
 def get_value(key, lists):
     return list(map(lambda t: t.get(key), lists))
 
+
 print(get_value('name', towns))
 print(get_value('population', towns))
