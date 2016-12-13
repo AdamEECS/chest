969612793a18bbe8c69b3d0cc694558776279172
Suzumiya <jrgwyz@163.com>
2016-12-13 11:14:18 +0800

    format

--- decode_pc_weichat.py
+++ decode_pc_weichat.py
@@ -4,9 +4,11 @@ import re
 def _decode_pc_dat(datfile):
     with open(datfile, 'rb') as f:
         buf = bytearray(f.read())
+
     magic = 0xff ^ list(buf)[0] if buf else 0x00
     print(magic)
     imgfile = re.sub(r'.dat$', '.jpg', datfile)
+    
     with open(imgfile, 'wb') as f:
         newbuf = bytearray(map(lambda b: b ^ magic, list(buf)))
         f.write(newbuf)
