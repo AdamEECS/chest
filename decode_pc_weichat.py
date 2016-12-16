7e109ebeaa18d8fdb84808cc45da00c5380bb4b0
Suzumiya <jrgwyz@163.com>
2016-12-17 00:01:05 +0800

    bug fixed

--- decode_pc_weichat.py
+++ decode_pc_weichat.py
@@ -6,9 +6,9 @@ def _decode_pc_dat(datfile):
         buf = bytearray(f.read())
 
     magic = 0xff ^ list(buf)[0] if buf else 0x00
-    print(magic)
+    # print(magic)
     imgfile = re.sub(r'.dat$', '.jpg', datfile)
-    
+
     with open(imgfile, 'wb') as f:
         newbuf = bytearray(map(lambda b: b ^ magic, list(buf)))
         f.write(newbuf)
