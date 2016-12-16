ba2ba6cfad497fa2357e0acd3e70095a86d17c21
Suzumiya <jrgwyz@163.com>
2016-12-16 23:58:58 +0800

    format

--- crawler.py
+++ crawler.py
@@ -3,6 +3,7 @@ def topics_from_url(child='', parent='19776749'):
     url_query = '{}?{}'.format(url, query)
     page = requests.post(url_query, data=data, headers=headers)
     dict = page.json()
+    
     for i in dict['msg'][1]:
         if i[0][0] == 'topic':
             t = Topic(topic_id=i[0][2], topic=i[0][1], fid=dict['msg'][0][2])
