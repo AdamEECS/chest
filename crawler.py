2f9b6411d79c121bb593b2bb8d90ba17c25ebf6c
Suzumiya <jrgwyz@163.com>
2016-12-23 17:27:24 +0800

    format

--- crawler.py
+++ crawler.py
@@ -5,7 +5,6 @@ def topics_from_url(child='', parent='19776749'):
     dict = page.json()
 
     for i in dict['msg'][1]:
-
         if i[0][0] == 'topic':
             t = Topic(topic_id=i[0][2], topic=i[0][1], fid=dict['msg'][0][2])
             print(t)
@@ -13,7 +12,7 @@ def topics_from_url(child='', parent='19776749'):
 
         if i[0][0] == 'load':
             topics_from_url(child=i[0][2], parent=dict['msg'][0][2])
-            
+
         if len(i[1]) > 0:
             topics_from_url(parent=i[1][0][0][3])
     return None
